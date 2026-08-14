b075b542f8fb3dc31506a6971e877ead377d062c
Mario Asenjo Pérez <masenjo@student.42madrid.com>
2026-08-14 16:38:23 +0200

    refactor: keep coder lifecycle focused

--- coders/coder.c
+++ coders/coder.c
@@ -6,7 +6,7 @@
 /*   By: masenjo <masenjo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/02 00:00:00 by masenjo           #+#    #+#             */
-/*   Updated: 2026/08/14 16:30:00 by masenjo          ###   ########.fr       */
+/*   Updated: 2026/08/14 16:40:00 by masenjo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,28 +20,6 @@ static int	cx_should_stop(t_sim *sim)
 	stop = sim->stop;
 	pthread_mutex_unlock(&sim->state_lock);
 	return (stop);
-}
-
-static void	cx_single_coder(t_coder *coder)
-{
-	t_sim	*sim;
-	int		taken;
-
-	sim = coder->sim;
-	taken = 0;
-	pthread_mutex_lock(&sim->state_lock);
-	pthread_mutex_lock(&sim->dongles[0].lock);
-	if (!sim->stop && sim->dongles[0].owner_id == 0)
-	{
-		sim->dongles[0].owner_id = coder->id;
-		taken = 1;
-	}
-	pthread_mutex_unlock(&sim->dongles[0].lock);
-	pthread_mutex_unlock(&sim->state_lock);
-	if (taken)
-		cx_log_state(sim, coder->id, "has taken a dongle");
-	while (!cx_should_stop(sim))
-		cx_sleep_ms(1);
 }
 
 static int	cx_mark_compile_done(t_coder *coder)
