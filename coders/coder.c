87ece7662526cd9b4a0ce721c29ca9ad8b7f8a50
Mario Asenjo Pérez <masenjo@student.42madrid.com>
2026-08-14 16:35:32 +0200

    fix: model the single coder with a real thread

--- coders/coder.c
+++ coders/coder.c
@@ -6,7 +6,7 @@
 /*   By: masenjo <masenjo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/02 00:00:00 by masenjo           #+#    #+#             */
-/*   Updated: 2026/07/02 00:00:00 by masenjo          ###   ########.fr       */
+/*   Updated: 2026/08/14 16:30:00 by masenjo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,28 @@ static int	cx_should_stop(t_sim *sim)
 	stop = sim->stop;
 	pthread_mutex_unlock(&sim->state_lock);
 	return (stop);
+}
+
+static void	cx_single_coder(t_coder *coder)
+{
+	t_sim	*sim;
+	int		taken;
+
+	sim = coder->sim;
+	taken = 0;
+	pthread_mutex_lock(&sim->state_lock);
+	pthread_mutex_lock(&sim->dongles[0].lock);
+	if (!sim->stop && sim->dongles[0].owner_id == 0)
+	{
+		sim->dongles[0].owner_id = coder->id;
+		taken = 1;
+	}
+	pthread_mutex_unlock(&sim->dongles[0].lock);
+	pthread_mutex_unlock(&sim->state_lock);
+	if (taken)
+		cx_log_state(sim, coder->id, "has taken a dongle");
+	while (!cx_should_stop(sim))
+		cx_sleep_ms(1);
 }
 
 static int	cx_mark_compile_done(t_coder *coder)
@@ -38,7 +60,6 @@ static int	cx_mark_compile_done(t_coder *coder)
 		{
 			sim->stop = 1;
 			stop = 1;
-			pthread_cond_broadcast(&sim->state_changed);
 		}
 	}
 	pthread_mutex_unlock(&sim->state_lock);
@@ -70,6 +91,8 @@ void	*cx_coder_routine(void *arg)
 	t_request	request;
 
 	coder = (t_coder *)arg;
+	if (coder->sim->cfg.number_of_coders == 1)
+		return (cx_single_coder(coder), NULL);
 	while (!cx_should_stop(coder->sim))
 	{
 		if (!cx_coder_wait_turn(coder, &request))
