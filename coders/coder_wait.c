7d1dc515829b3138269fb4f19aed321ce7ea5987
Mario Asenjo Pérez <masenjo@student.42madrid.com>
2026-08-14 16:35:03 +0200

    refactor: arbitrate contention per dongle

--- coders/coder_wait.c
+++ coders/coder_wait.c
@@ -6,7 +6,7 @@
 /*   By: masenjo <masenjo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/02 00:00:00 by masenjo           #+#    #+#             */
-/*   Updated: 2026/07/02 00:00:00 by masenjo          ###   ########.fr       */
+/*   Updated: 2026/08/14 16:30:00 by masenjo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,30 +22,91 @@ static int	cx_right_dongle(t_coder *coder)
 	return (coder->id % coder->sim->cfg.number_of_coders);
 }
 
-static int	cx_dongles_available(t_coder *coder)
+static void	cx_lock_pair(t_sim *sim, int left, int right)
+{
+	if (left < right)
+	{
+		pthread_mutex_lock(&sim->dongles[left].lock);
+		pthread_mutex_lock(&sim->dongles[right].lock);
+	}
+	else
+	{
+		pthread_mutex_lock(&sim->dongles[right].lock);
+		pthread_mutex_lock(&sim->dongles[left].lock);
+	}
+}
+
+static void	cx_unlock_pair(t_sim *sim, int left, int right)
+{
+	pthread_mutex_unlock(&sim->dongles[left].lock);
+	pthread_mutex_unlock(&sim->dongles[right].lock);
+}
+
+static int	cx_is_top(t_sim *sim, int index, int coder_id)
+{
+	t_request	top;
+
+	if (!cx_heap_peek(&sim->dongles[index].wait_heap, &top))
+		return (0);
+	return (top.coder_id == coder_id);
+}
+
+static int	cx_pair_available(t_coder *coder, int left, int right)
 {
 	t_sim	*sim;
 	long	now;
-	int		left;
-	int		right;
 
 	sim = coder->sim;
 	now = cx_now_ms() - sim->start_ms;
-	left = cx_left_dongle(coder);
-	right = cx_right_dongle(coder);
 	return (sim->dongles[left].owner_id == 0
 		&& sim->dongles[right].owner_id == 0
 		&& sim->dongles[left].available_at_ms <= now
 		&& sim->dongles[right].available_at_ms <= now);
 }
 
-static int	cx_is_my_turn(t_coder *coder, t_request *request)
+static int	cx_try_grant(t_coder *coder, t_request *request)
 {
-	t_request	top;
+	t_sim	*sim;
+	int		left;
+	int		right;
+	int		granted;
 
-	if (!cx_heap_peek(&coder->sim->wait_heap, &top))
+	sim = coder->sim;
+	left = cx_left_dongle(coder);
+	right = cx_right_dongle(coder);
+	cx_lock_pair(sim, left, right);
+	granted = cx_is_top(sim, left, coder->id)
+		&& cx_is_top(sim, right, coder->id)
+		&& cx_pair_available(coder, left, right);
+	if (granted)
+	{
+		cx_heap_pop(&sim->dongles[left].wait_heap, &sim->cfg, request);
+		cx_heap_pop(&sim->dongles[right].wait_heap, &sim->cfg, request);
+		sim->dongles[left].owner_id = coder->id;
+		sim->dongles[right].owner_id = coder->id;
+	}
+	cx_unlock_pair(sim, left, right);
+	return (granted);
+}
+
+static int	cx_queue_request(t_coder *coder, t_request *request)
+{
+	t_sim	*sim;
+	int		left;
+	int		right;
+
+	sim = coder->sim;
+	left = cx_left_dongle(coder);
+	right = cx_right_dongle(coder);
+	request->coder_id = coder->id;
+	request->seq = sim->request_seq++;
+	request->deadline_ms = coder->last_compile_start_ms
+		+ sim->cfg.time_to_burnout;
+	if (!cx_heap_push(&sim->dongles[left].wait_heap, &sim->cfg, *request))
 		return (0);
-	return (top.coder_id == request->coder_id && cx_dongles_available(coder));
+	if (!cx_heap_push(&sim->dongles[right].wait_heap, &sim->cfg, *request))
+		return (0);
+	return (1);
 }
 
 int	cx_coder_wait_turn(t_coder *coder, t_request *request)
@@ -54,13 +115,9 @@ int	cx_coder_wait_turn(t_coder *coder, t_request *request)
 
 	sim = coder->sim;
 	pthread_mutex_lock(&sim->state_lock);
-	request->coder_id = coder->id;
-	request->seq = sim->request_seq++;
-	request->deadline_ms = coder->last_compile_start_ms
-		+ sim->cfg.time_to_burnout;
-	if (!cx_heap_push(&sim->wait_heap, &sim->cfg, *request))
+	if (!cx_queue_request(coder, request))
 		return (pthread_mutex_unlock(&sim->state_lock), 0);
-	while (!sim->stop && !cx_is_my_turn(coder, request))
+	while (!sim->stop && !cx_try_grant(coder, request))
 	{
 		pthread_mutex_unlock(&sim->state_lock);
 		cx_sleep_ms(1);
@@ -68,8 +125,6 @@ int	cx_coder_wait_turn(t_coder *coder, t_request *request)
 	}
 	if (sim->stop)
 		return (pthread_mutex_unlock(&sim->state_lock), 0);
-	cx_heap_pop(&sim->wait_heap, &sim->cfg, request);
-	sim->dongles[cx_left_dongle(coder)].owner_id = coder->id;
-	sim->dongles[cx_right_dongle(coder)].owner_id = coder->id;
-	return (pthread_mutex_unlock(&sim->state_lock), 1);
+	pthread_mutex_unlock(&sim->state_lock);
+	return (1);
 }
