cadbb8c88d02085c3f5a1de95abe041fa7d4517a
Mario Asenjo Pérez <masenjo@student.42madrid.com>
2026-08-14 16:35:14 +0200

    refactor: protect dongle release with per-resource mutexes

--- coders/coder_release.c
+++ coders/coder_release.c
@@ -6,7 +6,7 @@
 /*   By: masenjo <masenjo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/02 00:00:00 by masenjo           #+#    #+#             */
-/*   Updated: 2026/07/02 00:00:00 by masenjo          ###   ########.fr       */
+/*   Updated: 2026/08/14 16:30:00 by masenjo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,49 @@ static int	cx_right_dongle(t_coder *coder)
 	return (coder->id % coder->sim->cfg.number_of_coders);
 }
 
+static void	cx_lock_dongles(t_sim *sim, int left, int right)
+{
+	if (left == right)
+		pthread_mutex_lock(&sim->dongles[left].lock);
+	else if (left < right)
+	{
+		pthread_mutex_lock(&sim->dongles[left].lock);
+		pthread_mutex_lock(&sim->dongles[right].lock);
+	}
+	else
+	{
+		pthread_mutex_lock(&sim->dongles[right].lock);
+		pthread_mutex_lock(&sim->dongles[left].lock);
+	}
+}
+
+static void	cx_unlock_dongles(t_sim *sim, int left, int right)
+{
+	pthread_mutex_unlock(&sim->dongles[left].lock);
+	if (left != right)
+		pthread_mutex_unlock(&sim->dongles[right].lock);
+}
+
 void	cx_coder_release(t_coder *coder)
 {
 	t_sim	*sim;
 	long	next;
+	int		left;
+	int		right;
 
 	sim = coder->sim;
+	left = cx_left_dongle(coder);
+	right = cx_right_dongle(coder);
 	pthread_mutex_lock(&sim->state_lock);
+	cx_lock_dongles(sim, left, right);
 	next = cx_now_ms() - sim->start_ms + sim->cfg.dongle_cooldown;
-	sim->dongles[cx_left_dongle(coder)].owner_id = 0;
-	sim->dongles[cx_right_dongle(coder)].owner_id = 0;
-	sim->dongles[cx_left_dongle(coder)].available_at_ms = next;
-	sim->dongles[cx_right_dongle(coder)].available_at_ms = next;
-	pthread_cond_broadcast(&sim->state_changed);
+	sim->dongles[left].owner_id = 0;
+	sim->dongles[left].available_at_ms = next;
+	if (left != right)
+	{
+		sim->dongles[right].owner_id = 0;
+		sim->dongles[right].available_at_ms = next;
+	}
+	cx_unlock_dongles(sim, left, right);
 	pthread_mutex_unlock(&sim->state_lock);
 }
